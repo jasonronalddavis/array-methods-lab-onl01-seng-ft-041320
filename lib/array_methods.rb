19a29a06d74184ef86e339b36f11874fc5b4b13b
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 18:14:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
 element.include?("wow")
-element.include?("Tardis")  
+  => true
+element.include?("Tardis")
+  => false
 end
 
 def using_sort(array)
