aacdea123f7016fca82fac998c0526a4eabf026d
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 18:38:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.include?("wow")
+.include? wow
 = true
-array.include?("Tardis")
+.include? Tardis
 = false
 end
 
